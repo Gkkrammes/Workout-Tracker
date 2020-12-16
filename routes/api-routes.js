13970df8669ba37d1bceb8c0f67016e44cca582c
Gretchen Krammes <gkkrammes@gmail.com>
2020-12-15 21:00:28 -0500

    functioning now

---
+++ routes/api-routes.js
@@ -0,0 +1,37 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+    app.get("/", (req, res) => {
+        res.send(index.html);
+    });
+
+    app.get("/api/workouts", function(req, res) {
+        db.Workout.find({}).then(function(data) {
+            res.json(data);
+        });
+    });
+
+    app.get("/api/workouts/range", function(req, res) {
+        db.Workout.find({}).then(function(data) {
+            res.json(data);
+        });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.updateOne({ _id: req.params.id }, {exercises: [req.body]}).then(function(updatedData) {
+            res.json(updatedData);
+          });
+      });
+
+      app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body)
+        .then(newdata => {
+            console.log(newdata + "post newdata ");
+            res.json(newdata);
+          })
+          .catch(({ message }) => {
+            console.log(message);
+          });
+      });
+};
